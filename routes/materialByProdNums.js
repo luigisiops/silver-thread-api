a9d1a2fe29ebc5a98266c3b2fc67532f8d63e371
ChapmanChappelle <chapman.chappelle@gmail.com>
2020-12-21 10:14:31 -0500

    Update materialByProdNums.js

--- routes/materialByProdNums.js
+++ routes/materialByProdNums.js
@@ -2,8 +2,11 @@ const express = require("express")
 const router = express.Router()
 const models = require("../models")
 
+// get all materialByProdNum
 
-
+router.get("/:productId/", (req, res) => {
+   let productId = req.body.id
+})
 
 router.post
 
