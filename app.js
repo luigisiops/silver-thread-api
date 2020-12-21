f156035773d72d2f830e0bc42634935f94488037
S-Philp <shannel.philp@yahoo.com>
2020-12-21 13:31:03 -0600

    Edited App.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express")
 const app = express()
-const bodyParser = require("body-parser")
+// const bodyParser = require("body-parser")
 const PORT = 8000
 const router = express.Router()
 const cors = require("cors")
+const models = require("./models")
+
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-const models = require("./models")
 app.use(cors())
 
 // routes
-const salesRouter = require("./routes/sales")
+const salesRouter = require("./routes/saleRoutes")
 app.use("/sales", salesRouter)
-const materialByProdNumsRouter = require("./routes/materialByProdNums")
+const materialByProdNumsRouter = require("./routes/materialByProdNumsRoutes")
 app.use("/materialByProdNums", materialByProdNumsRouter) 
 
 const materialRoutes = require("./routes/materialRoutes")
