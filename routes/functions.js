2eb69c981ea13ad8b3f8e9166a6d82d7cc37240e
Katy Donoghue <kathrynjdonoghue@gmail.com>
2021-01-04 08:58:31 -0500

    added function file

---
+++ routes/functions.js
@@ -0,0 +1,110 @@
+const models = require("../models");
+
+const addToMaterialsByProductNumber = (id, materials) => {
+
+    //build materials object
+    let addMaterials = materials.map(item => {
+        return {
+            product_id: parseInt(id),
+            ProductId: parseInt(id),
+            material_id: parseInt(item.material_id),
+            MaterialId: parseInt(item.material_id),
+            material_name: item.material_name,
+            material_unit_amount: parseInt(item.material_unit_amount),
+            material_cost: (item.unit_price)
+        }
+    })   
+    //add all to material by product number table
+    models.MaterialByProdNums.bulkCreate(addMaterials, { returning: false })
+        .then((savedAddMaterials) => {
+            console.log('saved material by product')
+        })
+}
+
+const deleteMaterialByProductNum = async (id) => {
+    let product_id = id
+
+    await models.MaterialByProdNums.destroy({
+        where: {
+            product_id: product_id
+        }
+    }).then(() => {
+        return true
+    }).catch(() => {
+        return false
+    })
+}
+
+const calculateWholesaleCosts = (labor, materialList) => {
+    //Calc labor per minute by diving $25 per hour by 60 minutes
+    let RatePerMinute = (25 / 60)
+
+    let laborCost = (labor * RatePerMinute)
+
+    if (materialList.length >= 1) {
+
+        //get list of extended costs
+        let materialPriceList = materialList.map(item => {
+            return (item.unit_price * item.material_unit_amount)
+        })
+
+        //add all material costs together
+        let totalMaterialCost = materialPriceList.reduce((total, amount) => total + amount)
+
+        //double material costs for pricing & add with labor
+        let costs = ((totalMaterialCost * 2) + laborCost)
+
+        //markup by 10% for utilities - rounded to .00
+        let totalWholesaleCosts = (costs * 1.1).toFixed(2)
+
+        return totalWholesaleCosts
+
+    } else {
+        //client would like a minimum of $3 labor cost for all products
+        if (laborCost <= 3) {
+            let laborMarkup = (3 * 1.1).toFixed(2)
+            return laborMarkup
+        } else {
+            let laborMarkup = (laborCost * 1.1).toFixed(2)
+            return laborMarkup
+        }
+    }
+
+}
+
+const updateWholesaleCost = (labor, materialList) => {
+    //Calc labor per minute by diving $25 per hour by 60 minutes
+    let RatePerMinute = (25 / 60)
+
+    let laborCost = (labor * RatePerMinute)
+
+    if (materialList.length >= 1) {
+
+        //get list of extended costs & add together
+        let materialPriceList = materialList.map(item => {
+            return (item.material_cost * item.material_unit_amount)
+        })
+
+        let totalMaterialCost = materialPriceList.reduce((total, amount) => total + amount)
+
+        //double material costs for pricing & add to labor
+        let costs = ((totalMaterialCost * 2) + laborCost)
+
+        //markup by 10% for utilities - rounded to .00
+        let totalWholesaleCosts = (costs * 1.1).toFixed(2)
+
+        return totalWholesaleCosts
+
+    } else {
+        //client would like a minimum of $3 labor cost for all products
+        if (laborCost <= 3) {
+            let laborMarkup = (3 * 1.1).toFixed(2)
+            return laborMarkup
+        } else {
+            let laborMarkup = (laborCost * 1.1).toFixed(2)
+            return laborMarkup
+        }
+    }
+}
+
+module.exports = { addToMaterialsByProductNumber, calculateWholesaleCosts, updateWholesaleCost, deleteMaterialByProductNum }  
