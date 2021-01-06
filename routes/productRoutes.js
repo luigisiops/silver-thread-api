38f3871d7c94d0756255bb0e71b0183f4033ba66
Katy Donoghue <kathrynjdonoghue@gmail.com>
2021-01-06 15:26:35 -0500

    made models a global variable

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const models = require("../models");
+//const models = require("../models");
 const functions = require('../functions/functions')
 
 router.get("/products", (req, res) => {
--- routes/saleRoutes.js
+++ routes/saleRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const models = require("../models")
+//const models = require("../models")
 const { Op } = require("sequelize")
 const functions = require('../functions/salesFunctions')
 
